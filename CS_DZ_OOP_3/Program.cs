c675342b430687386f0164ab813a6d29801cfe31
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-24 15:51:52 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_OOP_3/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_OOP_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+
+            List<Player> players = new List<Player> { };
+
+            Console.WriteLine("Это база данных игронков. Выберите что хотите сделать - ");
+            while (isWork)
+            {
+                Console.WriteLine("Для вывода списка игроков введите 1");
+                Console.WriteLine("Для добавления игрока введите 2");
+                Console.WriteLine("Для бана игрока введите 3");
+                Console.WriteLine("Для разбана игрока введите 4");
+                Console.WriteLine("Для выхода введите 5 или exit");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        ShowInfo(players);
+                        break;
+                    case "2":
+                        AddPlayer(ref players);
+                        break;
+                    case "3":
+                        Banned(players);
+                        break;
+                    case "4":
+                        DeBanned(players);
+                        break;
+                    case "5":
+                    case "exit":
+                        isWork = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка! Введены не верные данные.");
+                        break;
+                }
+            }
+        }
+        static void ShowInfo(List<Player> players)
+        {
+            Console.Clear();
+
+            if(players.Count > 0)
+            {
+                foreach (var player in players)
+                {
+                    Console.WriteLine("Уникальный номер игрока - " + player.NumberID + " его имя - " + player.NickName + " его уровень - " + player.Level + " бан - " + player.IsBanned);
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("В базе еще нет игроков!");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void Banned (List<Player> players)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите уникальный номер игрока для бана - ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var player in players)
+            {
+                if(player.NumberID == userInput)
+                {
+                    player.IsBanned = true;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void DeBanned(List<Player> players)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите уникальный номер игрока для разбана - ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var player in players)
+            {
+                if (player.NumberID == userInput)
+                {
+                    player.IsBanned = false;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void AddPlayer(ref List<Player> players)
+        {
+            Console.WriteLine("Введите уникальный номер игрока: ");
+            int playerID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите имя игрока: ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Введите его уровень: ");
+            int playerLevel = Convert.ToInt32(Console.ReadLine());
+            bool isBanned = false;
+
+            List<Player> player = new List<Player> { new Player(playerID, playerName, playerLevel, isBanned) };
+
+            players = player.Union(players).ToList();
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+
+    class Player
+    {
+        public int NumberID { get; private set; }
+
+        public string NickName { get; private set; }
+
+        public int Level { get; private set; }
+
+        public bool IsBanned { get; set; }
+
+        public Player(int numberID, string nickName, int level, bool isBanned)
+        {
+            NumberID = numberID;
+            NickName = nickName;
+            Level = level;
+            IsBanned = isBanned;
+        }
+    }
+}
